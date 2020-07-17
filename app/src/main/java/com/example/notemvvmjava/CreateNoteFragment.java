5af080f927ea8f38af8abf09fe3a75a36fa99a75
AndrewBlack-hub <crys2014@mail.ru>
2020-07-17 14:20:53 +0300

    refactoring

--- app/src/main/java/com/example/notemvvmjava/CreateNoteFragment.java
+++ app/src/main/java/com/example/notemvvmjava/CreateNoteFragment.java
@@ -6,7 +6,6 @@ import androidx.activity.OnBackPressedCallback;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.Navigation;
 
